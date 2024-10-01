7704124f3794880a0b6c55b16deadc359fc77ea3
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2024-10-02 01:18:22 +0530

    some minor changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,26 +6,41 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { SelectTheme } from "./theme-toggler";
 import { Button } from "./ui/button";
-
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { MailIcon } from "lucide-react";
 
 export const Navbar = () => {
+
     const session = useSession();
     console.log(session?.data?.user);
     const router = useRouter();
+
+
     return (
         <>
             <div className=" z-[999] h-fit flex items-center justify-between">
                 <header className="flex items-center font-secondary border-neutral-900 justify-between">
-                    <aside className="flex items-center gap-[10px]">
+                    <aside className="hidden md:flex items-center gap-[10px]">
                         <p className="text-xl font-extrabold font-secondary">Merged<span className="text-primary">&</span>Share .</p>
+                    </aside>
+                    <aside className="flex md:hidden items-center gap-[10px]">
+                        <p className="text-xl font-extrabold font-secondary">M<span className="text-primary">&</span>S.</p>
                     </aside>
                 </header>
 
-                <aside className="flex items-center  gap-4">
+                <aside className="flex items-center gap-2">
                     <SelectTheme />
-                    <Button className=" bg-none">Star on Github</Button>
-                    <Button className=" bg-none">Contact Us</Button>
-                </aside >
+
+                    <Link href="https://github.com/amanbairagi30/merged-n-share" className="flex items-center cursor-pointer hover:bg-accent gap-2 border-2 px-2 py-1 rounded-md" target="_blank" rel="noopener noreferrer">
+                        <GitHubLogoIcon className="h-5 w-5" />
+                        <span className="hidden md:block">Star on Github</span>
+                    </Link>
+                    <Link href="mailto:amanbairagi1089@gmail.com" className="flex items-center cursor-pointer hover:bg-accent gap-2 border-2 px-2 py-1 rounded-md" target="_blank" rel="noopener noreferrer">
+                        <MailIcon className="h-5 w-5" />
+                        <span className="hidden md:block">Contact Us</span>
+                    </Link>
+
+                </aside>
             </div >
         </>
     )
--- src/components/users-avatars.tsx
+++ src/components/users-avatars.tsx
@@ -1,10 +1,22 @@
 import AvatarCircles from "./ui/avatar-circle";
 
 const avatarUrls = [
-    "https://avatars.githubusercontent.com/u/16860528",
-    "https://avatars.githubusercontent.com/u/20110627",
-    "https://avatars.githubusercontent.com/u/106103625",
-    "https://avatars.githubusercontent.com/u/59228569",
+    {
+        img: "https://avatars.githubusercontent.com/u/8079861",
+        href: "https://merged-n-share.vercel.app/profile/hkirat"
+    },
+    {
+        img: "https://avatars.githubusercontent.com/u/76874341",
+        href: "https://merged-n-share.vercel.app/profile/devsargam"
+    },
+    {
+        img: "https://avatars.githubusercontent.com/u/37402791",
+        href: "https://merged-n-share.vercel.app/profile/nimit9"
+    },
+    {
+        img: "https://avatars.githubusercontent.com/u/89733575",
+        href: "https://merged-n-share.vercel.app/profile/TanmayDhobale"
+    },
 ];
 
 export async function UsersAvatar() {
