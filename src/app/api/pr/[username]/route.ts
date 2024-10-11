e28a5e8034a67e36ec97a0994cd0bc59da255d42
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2024-10-11 15:08:40 +0530

    added some changes in pr route

--- src/app/api/pr/[username]/route.ts
+++ src/app/api/pr/[username]/route.ts
@@ -13,18 +13,27 @@ export async function GET(req: NextRequest, { params: { username } }: { params: 
                 username: username,
             },
             include: {
+                contributedOrgs: {
+                    select: {
+                        id: true,
+                        name: true,
+                        avatar_url: true,
+                    }
+                },
                 pullRequests: {
                     select: {
                         prURL: true,
                         avatar: true,
                         mergedAt: true,
                         id: true,
+                        prNumber: true,
+                        repoURL: true,
                         body: true,
                         prTitle: true,
-                        orgId: true,
                         userName: true,
                         org: {
                             select: {
+                                id: true,
                                 name: true,
                                 avatar_url: true,
                                 github_url: true,
@@ -34,14 +43,14 @@ export async function GET(req: NextRequest, { params: { username } }: { params: 
                 },
             }
         })
-        if(!userData){
+        if (!userData) {
             return NextResponse.json({ success: true, message: "We can't find any user with this username in the database" });
         }
 
         if (userData?.pullRequests.length === 0) {
             return NextResponse.json({ success: true, message: "No Pull requests found for this username , try saving the new prs from my-pr section in Merged&Share" });
         }
-        
+
         return NextResponse.json({ success: true, userData: userData, message: "Merged PRs with this  username has been found successfully  " });
     } catch (error) {
         console.error("Error creating pull requests:", error);
